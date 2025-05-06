ee64a2da72435a1412c5e2d9abf60149a44824be
Lili Azatyan <liliazatyan2000@gmail.com>
2025-05-06 12:36:59 +0400

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+// Scroll progress bar under header
+window.addEventListener('scroll', () => {
+    const scrollTop = window.scrollY;
+    const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+    const scrollPercent = (scrollTop / docHeight) * 100;
+  
+    const progressBar = document.querySelector('.scroll-progress');
+    progressBar.style.width = scrollPercent + '%';
+  });
+  
